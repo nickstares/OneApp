e95bb0c0de85f24e46405042a5d0268bcf30f6bf
Nick Stares <Nick Stares>
2015-03-07 11:42:12 -0800

    associations working

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ActiveRecord::Base
+  belongs_to :users
+  belongs_to :items
+  belongs_to :artists
+  belongs_to :categories
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :items
+  validates :first_name, :last_name, :password, :email, presence: true
+  validates :email, uniqueness: true
+  has_secure_password
 end
