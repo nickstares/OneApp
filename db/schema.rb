8698535b8c4306554a8eb601f96896b39360e2fa
Nick Stares <Nick Stares>
2015-03-08 12:33:52 -0700

    added url column to artists table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150308192904) do
+ActiveRecord::Schema.define(version: 20150308193242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150308192904) do
     t.text     "artist_description"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+    t.string   "url"
   end
 
   create_table "categories", force: :cascade do |t|
