f1e1f2857ae7548be28f117b0b4c180d0146a910
Camilo Ordonez <icoj@gmail.com>
2015-03-09 11:26:03 -0700

    Shopping Cart working

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,40 +1,12 @@
 class ItemsController < ApplicationController
    def index
       @artist = Artist.find(params[:artist_id])
-      items = @artist.items
-      
-      @ItemsToShow = []
-         items.each do |item|
-            if item.user_id == session[:user_id]
-               @ItemsToShow << item
-            elsif item.user_id == nil
-               @ItemsToShow << item
-            end   
-         end
-
-      # reservedByOtherUsers = []
-      #    items.each do |item|
-      #       unless item.reservation_time.nil?
-      #           reservedByOtherUsers << item
-      #       end 
-      #    end
-         
-         @items = @ItemsToShow
-         
+      @items = @artist.items
    end
 
    
-
-
-
-
-
-
-
-
    def show 
       @item = Item.find(params[:id])
-
    end
 
 
--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,22 +1,15 @@
 class ReservationsController < ApplicationController
    
-   
-
    def addToReservation
       item = Item.find(params[:id])      
       if item.user_id.nil?
          item.user_id = session[:user_id]   
          item.reservation_time = Time.now 
          item.save
-         
-
          redirect_to :back
       else
          flash[:notice] = "This Item is already reserved"
          redirect_to :back
       end
-
    end
-
-
 end
