20108bbfa57b3a14407e62fde19ec2fb32d70bb4
Cynthia Yeung <cyeung91@gmail.com>
2015-03-12 10:43:46 -0700

    Added javascript for dynamic css content for page divider

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,27 +2,18 @@ $(document).on('ready',function(){
 
 $(".hover").on ('mouseenter',function(){
     $(this).find(".color-overlay").css("visibility", "visible");
-  })
+  });
 
  $(".hover").on ('mouseleave',function(){
     $(this).find(".color-overlay").css("visibility", "hidden");
-  })
+  });
 
- // $('li#navigation').on('click', function(g) {  // Starting the Game!
- //    console.log("poop")
     var pgurl = window.location.href.substr(window.location.href.lastIndexOf("/")+1);
     $('li#navigation').each(function(){
-          if($(this).attr("href") == pgurl || $(this).attr("href") == '' )
+          if($(this).attr("href") == pgurl || $(this).attr("href") === '' )
           $(this).addClass("active");
-     })
+     });
+
+  var pageDivider = document.getElementsByClassName('.page-divider');
+  pageDivider.setAttribute('text-insert', 'New content here!');
 });
-
-
-
-// var pgurl = window.location.href.substr(window.location.href
-// .lastIndexOf("/")+1);
-//      $("#nav ul li a").each(function(){
-//           if($(this).attr("href") == pgurl || $(this).attr("href") == '' )
-//           $(this).addClass("active");
-//      })
-// });
