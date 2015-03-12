d8efc0af11090136bfb4aad91ed51154af66405f
Camilo Ordonez <icoj@gmail.com>
2015-03-11 21:31:13 -0700

    Nothing Important

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -6,32 +6,34 @@ class ChargesController < ApplicationController
 
   def create
     # Amount in cents
-    @amount = 2100
-    customer = Stripe::Customer.create(
-      :email => 'example@stripe.com',
-      :card  => params[:stripeToken]
-    )
-    charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => @amount,
-      :description => 'Rails Stripe customer',
-      :currency    => 'usd'
-    )
-    @status = charge.status
-    if @status == "succeeded"
-        @items = Item.all
-        @items.each do |item|
-           if item.user_id == current_deviseuser[:id]
-              item.purchased_date = Time.now
-              item.user_id = current_deviseuser[:id]
-              item.reservation_time = nil
-              item.save
-           end
-        end
-    purchased    
-    end
-    redirect_to :purchased
+    
+      @amount = 2100
+      customer = Stripe::Customer.create(
+        :email => 'example@stripe.com',
+        :card  => params[:stripeToken]
+      )
+      charge = Stripe::Charge.create(
+        :customer    => customer.id,
+        :amount      => @amount,
+        :description => 'Rails Stripe customer',
+        :currency    => 'usd'
+      )
+      @status = charge.status
+      if @status == "succeeded"
+          @items = Item.all
+          @items.each do |item|
+             if item.user_id == current_deviseuser[:id]
+                item.purchased_date = Time.now
+                item.user_id = current_deviseuser[:id]
+                item.reservation_time = nil
+                item.save
+             end
+          end
+          purchased    
+      end
+      redirect_to :purchased
   end  
+
   
 
   def purchased
@@ -43,7 +45,6 @@ class ChargesController < ApplicationController
              @Purchaseditems << item       
          end
       end   
-
   end
 
 
